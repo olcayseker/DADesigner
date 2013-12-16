f49356f516bf7391654fa807f11b81d54981e2e3
olcayseker <seker.olcay@gmail.com>
2013-12-16 22:17:05 +0200

    added designer API manager folder

---
+++ DADesignerCore/DesignerAPIManager/CreateRootClass.cs
@@ -0,0 +1,29 @@
+﻿
+using DADesignerCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DADesignerCore.DesignerAPIManager
+{
+    public class CreateRootClass:ICreateRoot
+    {
+        ManagementPack _managementPack;
+        public CreateRootClass(ManagementPack managementPack)
+        {
+            _managementPack = managementPack;
+        }
+        public void CreateClass(ClassType rootClass)    
+        {
+            _managementPack.TypeDefinitions.EntityTypes.ClassTypes.Add(rootClass);
+        }
+
+        public void CreateDisplayString(string displayName)
+        {
+            DisplayString displayString = new DisplayString() { Name = displayName };
+            _managementPack.LanguagePacks.LanguagePack.DisplayStrings.Add(displayString);
+        }
+    }
+}
