94aa64629ffa8843a3ec5675521b07d79e88768e
olcayseker <seker.olcay@gmail.com>
2013-12-16 22:16:00 +0200

    added new controls

--- DADesignerFormsGUI/Form1.Designer.cs
+++ DADesignerFormsGUI/Form1.Designer.cs
@@ -30,15 +30,27 @@
         {
             this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cbAvailabilityState = new System.Windows.Forms.CheckBox();
+            this.cbPerformanceState = new System.Windows.Forms.CheckBox();
+            this.cbSecurityState = new System.Windows.Forms.CheckBox();
+            this.cbConfigurationState = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeviewContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addNodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.treeviewContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.treeviewContextMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -49,6 +61,7 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
             this.splitContainer1.Panel1.Controls.Add(this.treeView1);
             // 
             // splitContainer1.Panel2
@@ -58,14 +71,144 @@
             this.splitContainer1.SplitterDistance = 246;
             this.splitContainer1.TabIndex = 0;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.comboBox4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cbAvailabilityState, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cbPerformanceState, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cbSecurityState, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cbConfigurationState, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 285);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(246, 147);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "WorstOf",
+            "BestOf"});
+            this.comboBox4.Location = new System.Drawing.Point(126, 111);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(105, 21);
+            this.comboBox4.TabIndex = 7;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "WorstOf",
+            "BestOf"});
+            this.comboBox3.Location = new System.Drawing.Point(126, 75);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(105, 21);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "WorstOf",
+            "BestOf"});
+            this.comboBox2.Location = new System.Drawing.Point(126, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(105, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // cbAvailabilityState
+            // 
+            this.cbAvailabilityState.AutoSize = true;
+            this.cbAvailabilityState.Location = new System.Drawing.Point(3, 3);
+            this.cbAvailabilityState.Name = "cbAvailabilityState";
+            this.cbAvailabilityState.Size = new System.Drawing.Size(100, 17);
+            this.cbAvailabilityState.TabIndex = 0;
+            this.cbAvailabilityState.Tag = "AvailabilityState";
+            this.cbAvailabilityState.Text = "AvailabilityState";
+            this.cbAvailabilityState.UseVisualStyleBackColor = true;
+            // 
+            // cbPerformanceState
+            // 
+            this.cbPerformanceState.AutoSize = true;
+            this.cbPerformanceState.Location = new System.Drawing.Point(3, 39);
+            this.cbPerformanceState.Name = "cbPerformanceState";
+            this.cbPerformanceState.Size = new System.Drawing.Size(111, 17);
+            this.cbPerformanceState.TabIndex = 1;
+            this.cbPerformanceState.Tag = "PerformanceState";
+            this.cbPerformanceState.Text = "PerformanceState";
+            this.cbPerformanceState.UseVisualStyleBackColor = true;
+            // 
+            // cbSecurityState
+            // 
+            this.cbSecurityState.AutoSize = true;
+            this.cbSecurityState.Location = new System.Drawing.Point(3, 75);
+            this.cbSecurityState.Name = "cbSecurityState";
+            this.cbSecurityState.Size = new System.Drawing.Size(89, 17);
+            this.cbSecurityState.TabIndex = 2;
+            this.cbSecurityState.Tag = "SecurityState";
+            this.cbSecurityState.Text = "SecurityState";
+            this.cbSecurityState.UseVisualStyleBackColor = true;
+            // 
+            // cbConfigurationState
+            // 
+            this.cbConfigurationState.AutoSize = true;
+            this.cbConfigurationState.Location = new System.Drawing.Point(3, 111);
+            this.cbConfigurationState.Name = "cbConfigurationState";
+            this.cbConfigurationState.Size = new System.Drawing.Size(113, 17);
+            this.cbConfigurationState.TabIndex = 3;
+            this.cbConfigurationState.Tag = "ConfigurationState";
+            this.cbConfigurationState.Text = "ConfigurationState";
+            this.cbConfigurationState.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "WorstOf",
+            "BestOf"});
+            this.comboBox1.Location = new System.Drawing.Point(126, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(105, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
             // treeView1
             // 
             this.treeView1.ContextMenuStrip = this.treeviewContextMenu;
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Top;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(246, 432);
+            this.treeView1.Size = new System.Drawing.Size(246, 282);
             this.treeView1.TabIndex = 0;
+            this.treeView1.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.treeView1_AfterLabelEdit);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            this.treeView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyDown);
+            // 
+            // treeviewContextMenu
+            // 
+            this.treeviewContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNodeToolStripMenuItem});
+            this.treeviewContextMenu.Name = "treeviewContextMenu";
+            this.treeviewContextMenu.Size = new System.Drawing.Size(129, 26);
+            // 
+            // addNodeToolStripMenuItem
+            // 
+            this.addNodeToolStripMenuItem.Name = "addNodeToolStripMenuItem";
+            this.addNodeToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.addNodeToolStripMenuItem.Text = "Add Node";
+            this.addNodeToolStripMenuItem.Click += new System.EventHandler(this.addNodeToolStripMenuItem_Click);
             // 
             // propertyGrid1
             // 
@@ -91,11 +234,6 @@
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // treeviewContextMenu
-            // 
-            this.treeviewContextMenu.Name = "treeviewContextMenu";
-            this.treeviewContextMenu.Size = new System.Drawing.Size(61, 4);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -111,6 +249,9 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.treeviewContextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,6 +265,16 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ContextMenuStrip treeviewContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem addNodeToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox cbAvailabilityState;
+        private System.Windows.Forms.CheckBox cbPerformanceState;
+        private System.Windows.Forms.CheckBox cbSecurityState;
+        private System.Windows.Forms.CheckBox cbConfigurationState;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
