fddd1518f8e3e46e098b182647f0edd1f0249a97
olcayseker <seker.olcay@gmail.com>
2014-01-31 23:11:40 +0200

    Added base class

---
+++ DADesignerCore/Entities/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using DACore.ManagementPackElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DADesignerCore.Entities
+{
+    public abstract class BaseEntity
+    {
+
+        public BaseEntity(XElement rootElement)
+        {
+            RootElement = rootElement;
+        }
+     //   ManagementPackElement rootElement;// = new ManagementPackElement("ManagementPack");
+        protected XElement RootElement
+        {
+            get;
+            private set;
+        }
+
+        public abstract XElement CurrentElement { get; protected set; }
+    }
+}
